c1a440f1dfc6065a5f22d476b12f97ccccb508f3
Herinta Armantya <herintya@gmail.com>
2020-06-25 08:59:29 +0700

    yysy

--- src/App.js
+++ src/App.js
@@ -202,44 +202,8 @@ function App(){
                     <div className="tittle-line"><hr></hr></div>
                   </div>
                   <div className=" mt-3  wow animate__animated animate__zoomIn">
-                  <div className="content d-flex justify-content-between">
-                <Card className="card-small">
-                    <img src={ic_biaya}/>
-                    <Card.Body>
-                        <h5>Rp 100 M</h5>
-                        <h6>Biaya Tersalurkan</h6>
-                    </Card.Body>
-                </Card>
-                <Card className="card-small">
-                    <img src={ic_petani}/>
-                    <Card.Body>
-                        <h5>2000n</h5>
-                        <h6>Petani Terdaftar</h6>
-                    </Card.Body>
-                </Card>
-                <Card className="card-small">
-                    <img src={ic_investor}/>
-                    <Card.Body>
-                        <h5>5670</h5>
-                        <h6>Investor Terdaftar</h6>
-                    </Card.Body>
-                </Card>
-                <Card className="card-small">
-                    <img src={ic_desa}/>
-                    <Card.Body>
-                        <h5>500</h5>
-                        <h6>Desa Terdaftar</h6>
-                    </Card.Body>
-                </Card>
-                <Card className="card-small">
-                    <img src={ic_lahan}/>
-                    <Card.Body>
-                        <h5>600 ha</h5>
-                        <h6>Lahan Terdaftar</h6>
-                    </Card.Body>
-                </Card>
-            </div>
-              </div>
+                    <Pencapaian/>
+                  </div>
            </Container>
         </section>
 
--- src/Screens/PembiayaanPetani.jsx
+++ src/Screens/PembiayaanPetani.jsx
@@ -6,15 +6,6 @@ import { Card } from 'react-bootstrap';
 
 import {Navbar, Nav} from 'react-bootstrap';
 
-import logo from '../asset/image/logo.svg';
-import ic_call from '../asset/image/ic_call.svg';
-import ic_email from '../asset/image/ic_email.svg';
-import ic_insta from '../asset/image/ic_insta.svg';
-import ic_fb from '../asset/image/ic_fb.svg';
-import ic_youtube from '../asset/image/ic_youtube.svg';
-import ic_modal from '../asset/image/ic_modal.svg';
-
-import ic_facebook from '../asset/image/ic_facebook.svg'
 
 import ic_search from '../asset/image/ic_search.svg';
 
@@ -22,6 +13,7 @@ import kayumanis from '../asset/image/kayumanis.svg';
 import beras from '../asset/image/beras.svg';
 import jagung from '../asset/image/jagung.svg'
 import { Newnav, Myfooter } from '../Components/MyComponents';
+import Pembiayaan from '../Components/Pembiayaan';
 
 import '../asset/style.css';
 
@@ -30,219 +22,24 @@ class pembiayaan extends Component {
         return(
             <div className="Background-grey">
                     <Newnav/>
-                    <Container>
-                    <section className="pembiayaan">
-                        
-                        <div className=" ">
-                        <div className="content d-flex justify-content-between">
-                            <Card className="card card-pembiayaan">
-                                    <img src={kayumanis}/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Kayu Manis</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="/deskripsi-pembiayaan" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-                                <Card className="card card-pembiayaan">
-                                    <img src={beras}/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Card title</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="#" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-                                <Card className="card card-pembiayaan">
-                                    <img src={jagung} alt=""/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Card title</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="#" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-
-                            </div>
-                        </div>
-        
-                        
-                    </section>
-                    <section className="pembiayaan">
-                        
-                        <div className=" ">
-                        <div className="content d-flex justify-content-between">
-                            <Card className="card card-pembiayaan">
-                                    <img src={kayumanis}/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Kayu Manis</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="/deskripsi-pembiayaan" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-                                <Card className="card card-pembiayaan">
-                                    <img src={beras}/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Card title</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="#" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-                                <Card className="card card-pembiayaan">
-                                    <img src={jagung} alt=""/>
-                                    <div className="tittle-card">
-                                        <h5 className="card-title">Card title</h5>
-                                    </div>
-                                    <div className="list-group list-group-flush">
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Harga</h6>
-                                                <p>Rp.2.000.000</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Bagi Hasil</h6>
-                                                <p>20%/tahun</p>
-                                            </div>
-                                        </div>
-                                        <div className="list-group-item d-flex">
-                                            <div className="item">
-                                                <h6>Stok</h6>
-                                                <p>34 slot</p>
-                                            </div>
-                                            <div className="item">
-                                                <h6>Tersisa</h6>
-                                                <p>12 slot</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div className="tittle-card">
-                                        <button href="#" className="btn-green mt-1 ml-6">Lihat Pembiayaan</button>
-                                    </div>
-                                </Card>
-
-                            </div>
-                        </div>
-        
-                        
-                    </section>
+                <Container>
+                <section className="pembiayaan">
+                    
+                    <div className=" mt-3  wow animate__animated animate__zoomIn">
+                    <Pembiayaan/>
+                    </div>
+                   
+                    
+                </section>
+                <section className="pembiayaan">
+                    
+                    <div className=" mt-3  wow animate__animated animate__zoomIn">
+                    <Pembiayaan/>
+                    </div>
+                   
+                    
+                </section>
+                    
                 </Container>
                 <Myfooter/>
             </div>
